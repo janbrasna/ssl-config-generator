2b1086f381b1b52d77ac7c97969b7e18c19ecac9
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2024-03-20 14:01:05 +0100

    Bump nginx version

--- src/js/configs.js
+++ src/js/configs.js
@@ -104,7 +104,7 @@ module.exports = {
   nginx: {
     checked: true,
     highlighter: 'nginx',
-    latestVersion: '1.25.3',
+    latestVersion: '1.25.4',
     name: 'nginx',
     tls13: '1.13.0',
   },
