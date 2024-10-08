aa81ef58a788982ca3b3f145deeed45a56adea94
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2024-10-08 17:40:52 +0200

    Update Tomcat v9.x version

--- src/js/configs.js
+++ src/js/configs.js
@@ -187,7 +187,7 @@ module.exports = {
   },
 tomcat: {
     highlighter: 'xml',
-    latestVersion: '9.0.85',
+    latestVersion: '9.0.96',
     name: 'Tomcat',
     supportsHsts: true,
     supportsOcspStapling: false,
